e2b9444ce2c28135ccadf61bef228c7198537a59
Ivan Nicoli <inmjava@yahoo.com.br>
2020-01-05 11:02:00 -0300

    mudando para o tema dark

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import './styles.css'
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import {
+  Form, Dropdown, DropdownButton,
+  FormControl, InputGroup, Col,
+  Row, Container, Navbar, Nav,
+  NavDropdown
+} from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,96 +15,146 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div id="container" class="container-fluid d-flex h-100 flex-column">
 
 
-        <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-            <div>
-              <h10 class="hash-heading">Algolia&nbsp;</h10>
-            </div>
-            <div class="aa-input-container" id="aa-input-container">
-              <input type="search" id="aa-search-input" class="form-control text-input" placeholder="Pesquisar por músicas e momentos..." name="search" autocomplete="off" />
-            </div>
-          </div>
-        </div>
+      <Container fluid="true">
+
+        <Row>
+          <Col>
+            <Navbar expand="lg">
+              <Navbar.Brand href="#home">Vem Louvar</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <NavDropdown title="Missa" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Cadastro Músicas</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Missas</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Sobre</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form inline>
+                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Navbar>
+          </Col>
+
+        </Row>
+        <Row>
+          <Col>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text variant="success" id="basic-addon1">Algolia</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Col>
+
+        </Row>
 
 
-        <div class="row" style={{ display: "none" }}>
-          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <div>
-              <h10 class="hash-heading">Momentos&nbsp;</h10>
-            </div>
-            <select id="momentos" class="form-control text-input">
-            </select>
-          </div>
-          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <div>
-              <h10 class="hash-heading">Musicas&nbsp;</h10>
-            </div>
-            <select id="musicas" class="form-control text-input">
-            </select>
-          </div>
-        </div>
+        <Row style={{ display: "none" }}>
+          <Col>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text variant="success" id="basic-addon1">Momentos</InputGroup.Text>
+              </InputGroup.Prepend>
+              <DropdownButton
+                as={InputGroup.Prepend}
+                variant="outoline-secondary"
+                title="Dropdown"
+                id="input-group-dropdown-1"
+              >
+                <Dropdown.Item href="#">Action</Dropdown.Item>
+                <Dropdown.Item href="#">Another action</Dropdown.Item>
+                <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#">Separated link</Dropdown.Item>
+              </DropdownButton>
+            </InputGroup>
+          </Col>
+          <Col>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text variant="success" id="basic-addon1">Músicas</InputGroup.Text>
+              </InputGroup.Prepend>
+              <DropdownButton
+                as={InputGroup.Prepend}
+                variant="outoline-secondary"
+                title="Dropdown"
+                id="input-group-dropdown-1"
+              >
+                <Dropdown.Item href="#">Action</Dropdown.Item>
+                <Dropdown.Item href="#">Another action</Dropdown.Item>
+                <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#">Separated link</Dropdown.Item>
+              </DropdownButton>
+            </InputGroup>
+          </Col>
+
+        </Row>
 
 
-        <div class="row" style={{ display: "none" }}>
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-            <div>
-              <h10 class="hash-heading">Missa&nbsp;</h10>
-            </div>
-            <ol class='musicas'>
-              <li>Primeira Música</li>
-              <li>Segunda Música</li>
-              <li>Terceira Música</li>
-            </ol>
-          </div>
-        </div>
+        <Row>
+          <Col>
+            <Form.Label>Missa</Form.Label>
+          </Col>
+        </Row>
 
 
-        <div class="row flex-fill d-flex justify-content-start">
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 portlet-container portlet-dropzone">
-            <div>
-              <h10 class="hash-heading">Missa&nbsp;</h10>
-            </div>
-            <div id="jsGrid"></div>
-          </div>
-        </div>
+        <Row>
+          <Col>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>Apresentação</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl as="textarea" aria-label="With textarea" rows="5" />
+            </InputGroup>
+          </Col>
+          <Col>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>Cifra</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl as="textarea" aria-label="With textarea" rows="5" />
+            </InputGroup>
+          </Col>
 
+        </Row>
 
-        <div class="row">
-          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 ">
-            <div>
-              <h10 class="hash-heading">Apresentação&nbsp;</h10>
-            </div>
-            <textarea class="form-control text-input" rows="5"></textarea>
-          </div>
-          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 ">
-            <div>
-              <h10 class="hash-heading">Cifra&nbsp;</h10>
-            </div>
-            <textarea class="form-control text-input" rows="5"></textarea>
-          </div>
-        </div>
+        <Row>
+          <Col>
+            <Form.Label>&nbsp;</Form.Label>
+          </Col>
+        </Row>
 
+        <Row>
+          <Col>
+          
+            <ButtonToolbar>
+              <Button variant="primary">Primary</Button>
+              <Button variant="secondary">Secondary</Button>
+              <Button variant="success">Success</Button>
+              <Button variant="warning">Warning</Button>
+              <Button variant="danger">Danger</Button>
+              <Button variant="info">Info</Button>
+              <Button variant="light">Light</Button>
+              <Button variant="dark">Dark</Button>
+              <Button variant="link">Link</Button>
+            </ButtonToolbar>
+          
+          </Col>
 
-        <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-            <div>
-              <h10 class="hash-heading">&nbsp;</h10>
-            </div>
-
-            <div class="btn-toolbar">
-              <button type="button" class="btn btn-secondary btn-pptx">Gerar Apresentação</button>&nbsp;&nbsp;&nbsp;
-              <button type="button" class="btn btn-secondary btn-docx">Gerar Cifra</button>&nbsp;&nbsp;&nbsp;
-              <button type="button" class="btn btn-secondary" onclick="window.location.href = './pages/cadastromusicas.html';">Cadastro Músicas</button>
-            </div>
-            <div>
-              <h10 class="hash-heading">&nbsp;</h10>
-            </div>
-          </div>
-        </div>
-      </div>
+        </Row>
+      </Container >
     );
   }
 }
