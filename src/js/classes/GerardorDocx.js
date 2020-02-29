835a8fff1a58089365ea253e1aab76e21b311a54
Ivan Nicoli <inmjava@yahoo.com.br>
2020-02-29 18:04:37 -0300

    agora vai docx man

--- src/js/classes/GerardorDocx.js
+++ src/js/classes/GerardorDocx.js
@@ -1,22 +1,11 @@
-const DEFAULT_TITLE_COLOR = "3F8A70";
-const DEFAULT_REFRAO_COLOR = "DB4B5E";
-const DEFAULT_NORMAL_COLOR = "000000";
-const DEFAULT_BACKGROUND_COLOR = "ffffff";
-
 export default class GeradorDocx {
 
-  constructor(musicas, titleColor = DEFAULT_TITLE_COLOR, refraoColor = DEFAULT_REFRAO_COLOR, normalColor = DEFAULT_NORMAL_COLOR, backgroundColor = DEFAULT_BACKGROUND_COLOR) {
+  constructor(musicas) {
     this.musicas = musicas;
-    this.titleColor = titleColor;
-    this.refraoColor = refraoColor;
-    this.normalColor = normalColor;
-    this.backgroundColor = backgroundColor;
   }
 
   gerarDocX = () => {
-    window.generate();
-  }
-
-
-  
+    alert("vai fela")
+    window.generate(this.musicas);
+  } 
 }
